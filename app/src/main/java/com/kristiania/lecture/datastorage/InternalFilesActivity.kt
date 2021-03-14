0cb5984935b5fdfcac7c9827d97e482cdf46dd54
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2021-03-14 22:37:05 +0100

    ready for live

--- app/src/main/java/com/kristiania/lecture/datastorage/InternalFilesActivity.kt
+++ app/src/main/java/com/kristiania/lecture/datastorage/InternalFilesActivity.kt
@@ -1,9 +1,7 @@
 package com.kristiania.lecture.datastorage
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kristiania.lecture.datastorage.databinding.ActivityCacheBinding
 
@@ -11,10 +9,6 @@ import com.kristiania.lecture.datastorage.databinding.ActivityCacheBinding
 class InternalFilesActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCacheBinding
-
-    val internalFileName = "my-internal-file"
-
-    // First lets make a function to manipulate files
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,40 +21,22 @@ class InternalFilesActivity : AppCompatActivity() {
         // button click listener to save the data to cache
         binding.btnSaveInternal.setOnClickListener {
             // Get file out stream
-
-            val fout = openFileOutput("my-internal-file", Context.MODE_PRIVATE)
-            fout.write(fileContent.toByteArray())
-            fout.close()
             // Write content to the file output stream
-
             // close the file stream
-
         }
 
         // Button click listener to read form the cache
         binding.btnReadInternal.setOnClickListener {
             // get all the files list from the internal folder
-            val list = fileList()
-            val myFile = list[0]
             // create an input stream for the file
-            val fIn = openFileInput(myFile)
             // Get the data
-            val string = fIn.bufferedReader().useLines { lines ->
-                lines.fold("") { append, text ->
-                    "$append\n$text"
-                }
-            }
 
-            Toast.makeText(this, string, Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, data, Toast.LENGTH_LONG).show()
         }
 
         // Button click listener to delete from the internal folder
         binding.btnDeleteInternal.setOnClickListener {
             // get all the files list from the internal folder and delete our
-            val list = fileList()
-            val myFile = list[0]
-            deleteFile(myFile)
-
         }
 
     }
--- app/src/main/java/com/kristiania/lecture/datastorage/SharedPreferenceActivity.kt
+++ app/src/main/java/com/kristiania/lecture/datastorage/SharedPreferenceActivity.kt
@@ -1,44 +1,35 @@
 package com.kristiania.lecture.datastorage
 
-import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kristiania.lecture.datastorage.databinding.ActivitySharedPreferenceBinding
 
 class SharedPreferenceActivity : AppCompatActivity() {
 
+    private lateinit var sharedPreference: SharedPreferences
     private lateinit var binding: ActivitySharedPreferenceBinding
-    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySharedPreferenceBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-
         // Create a shared preference instance
-        sharedPreferences =
-            getSharedPreferences("no.kristainia.android.preference", Context.MODE_PRIVATE)
 
         binding.btnSavePreference.setOnClickListener {
             // create editor and add the key value pair to the preference
-            val editor = sharedPreferences.edit()
-            editor.putString("KEY_STR", "Well it may work!")
-            editor.apply()
+
         }
 
         binding.btnReadPreference.setOnClickListener {
-            val str = sharedPreferences.getString("KEY_STR", null)
-            Toast.makeText(this, str, Toast.LENGTH_LONG).show()
+
         }
 
         binding.btnDeletePreference.setOnClickListener {
-            // create editor and remove the key value pair form the preference
-            val editor = sharedPreferences.edit()
-            editor.remove("KEY_STR")
-            editor.apply()
+            // create editor and remove the key value pair from the preference
+            // Remove all
+
         }
     }
 
